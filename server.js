36494e8578009f75fdd78d3b8b1715379f183efb
Mathieu Geukens <mr-zwets@protonmail.com>
2023-10-17 11:25:15 +0200

    fix issue where it loops updateMintingUtxos() on payout

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 import express from "express";
 import cors from "cors";
 import { ElectrumClient } from "electrum-cash";
-import { mintStatus, tokenId, contractAddressMint, mintDate, nrMintingUtxos, collectionSize, network, apiDomain, bcmrJSON, chaingraphUrl, hideIPFSImages, hideIPFSIcons } from "./config.js"
+import { mintStatus, tokenId, contractAddressMint, mintDate, nrMintingUtxos, collectionSize, network, apiDomain, bcmrJSON, chaingraphUrl, hideIPFSImages, hideIPFSIcons } from "./config.js";
 import { queryMintingNFTs } from './queryChainGraph.js';
 import { vmNumberToBigInt, hexToBin, bigIntToVmNumber, binToHex } from '@bitauth/libauth';
 
@@ -34,6 +34,7 @@ async function updateMintingUtxos() {
 
   if (responseJson.data) {
     const outputs = responseJson.data.output;
+    let txidChanged = false;
     for (let i = 0; i < outputs.length; i++) {
       const output = outputs[i];
       const txid = output.transaction_hash.slice(2);
@@ -41,6 +42,9 @@ async function updateMintingUtxos() {
       const newMintingUtxo = { txid, vout: output.output_index, commitment: commitmentScriptNum };
       const commitmentNumber = Number(vmNumberToBigInt(hexToBin(commitmentScriptNum)));
       const numberMintingUtxo = commitmentNumber % nrMintingUtxos;
+      const oldMintingUtxo = mintingUtxos[numberMintingUtxo];
+      const oldTxId = oldMintingUtxo?.txid;
+      if (oldTxId != txid) { txidChanged = true; };
       newMintingUtxos[numberMintingUtxo] = newMintingUtxo;
       const amountMinted = Math.floor(commitmentNumber / nrMintingUtxos);
       newMintedPerUtxo[numberMintingUtxo] = amountMinted;
@@ -49,7 +53,7 @@ async function updateMintingUtxos() {
     // We need to set newMintedPerUtxo to the max number of minted items for
     // any fully used minting UTXO. Otherwise the reduce below will not
     // be accurate near the end of the mint.
-    for (let i = 0; i < nrMintingUtxos; i++ ) {
+    for (let i = 0; i < nrMintingUtxos; i++) {
       if (newMintedPerUtxo[i] == undefined) {
         newMintedPerUtxo[i] = collectionSize / nrMintingUtxos;
       }
@@ -60,8 +64,9 @@ async function updateMintingUtxos() {
     mintedPerUtxo = newMintedPerUtxo;
     nftsMinted = mintedPerUtxo.reduce((a, b) => a + b, 0);
 
-    // If the Chaingraph instance has not seen the new minting tx, wait and re-fetch.
-    if (nftsMinted == oldAmountMinted && nftsMinted != 0) {
+    const noStateUpdate = nftsMinted == oldAmountMinted && !txidChanged;
+    // If the Chaingraph instance has not seen the new tx, wait and re-fetch.
+    if (noStateUpdate && nftsMinted != 0) {
       setTimeout(updateMintingUtxos, 500);
     }
   } else {
@@ -84,7 +89,7 @@ app.get('/', (req, res) => {
     nftsAvailable,
     mintStatus
   });
-})
+});
 
 // Minting UTXO information route.
 app.get('/mint', (req, res) => {
@@ -101,7 +106,7 @@ app.get('/mint', (req, res) => {
 
     // If we have UTXOs left, pick a revolving commitment.
     if (existingMintingUtxos.length > 0) {
-      let idx = commitmentCount % existingMintingUtxos.length
+      let idx = commitmentCount % existingMintingUtxos.length;
 
       availableMintingUtxo = existingMintingUtxos[idx].commitment;
       commitmentCount++;
@@ -114,28 +119,28 @@ app.get('/mint', (req, res) => {
     mintedPerUtxo,
     availableMintingUtxo
   });
-})
+});
 
 // Filters BCMR file to hide IPFS links and unminted items.
 const filteredBCMR = function (bcmr) {
-  let bcmrCopy = JSON.parse(JSON.stringify(bcmr))
+  let bcmrCopy = JSON.parse(JSON.stringify(bcmr));
 
   if (bcmrCopy.identities[tokenId]) {
     for (let i = 0; i < collectionSize; i++) {
-      let vmNum = bigIntToVmNumber(BigInt(i))
-      let indexHex = binToHex(vmNum)
+      let vmNum = bigIntToVmNumber(BigInt(i));
+      let indexHex = binToHex(vmNum);
 
       if (!nftMinted(i + 1)) {
-        delete bcmrCopy.identities[tokenId][mintDate]["token"]["nfts"]["parse"]["types"][indexHex]
+        delete bcmrCopy.identities[tokenId][mintDate]["token"]["nfts"]["parse"]["types"][indexHex];
       } else {
         if (hideIPFSImages) {
           if (bcmrCopy.identities[tokenId][mintDate]["token"]["nfts"]["parse"]["types"][indexHex]) {
-            bcmrCopy.identities[tokenId][mintDate]["token"]["nfts"]["parse"]["types"][indexHex]["uris"]["image"] = apiDomain + "images/" + (i + 1)
+            bcmrCopy.identities[tokenId][mintDate]["token"]["nfts"]["parse"]["types"][indexHex]["uris"]["image"] = apiDomain + "images/" + (i + 1);
           }
         }
         if (hideIPFSIcons) {
           if (bcmrCopy.identities[tokenId][mintDate]["token"]["nfts"]["parse"]["types"][indexHex]) {
-            bcmrCopy.identities[tokenId][mintDate]["token"]["nfts"]["parse"]["types"][indexHex]["uris"]["icon"] = apiDomain + "icons/" + (i + 1)
+            bcmrCopy.identities[tokenId][mintDate]["token"]["nfts"]["parse"]["types"][indexHex]["uris"]["icon"] = apiDomain + "icons/" + (i + 1);
           }
         }
       }
@@ -143,12 +148,12 @@ const filteredBCMR = function (bcmr) {
   }
 
   return bcmrCopy;
-}
+};
 
 // BCMR route.
 app.get('/.well-known/bitcoin-cash-metadata-registry.json', (req, res) => {
   res.json(filteredBCMR(bcmrJSON));
-})
+});
 
 // Checks if an NFT has been minted. Used as a gate by many endpoints.
 const nftMinted = function (id) {
@@ -162,65 +167,65 @@ const nftMinted = function (id) {
   let highMintId = mintedPerUtxo[threadID] * nrMintingUtxos;
 
   return id <= highMintId;
-}
+};
 
 // Sends an image for an NFT.
 const sendImage = function (id) {
   if (nftMinted(id)) {
-    return 'data/images/' + id + '.png'
+    return 'data/images/' + id + '.png';
   } else {
-    return 'images/unknown.webp'
+    return 'images/unknown.webp';
   }
-}
+};
 
 // NFT image route.
 app.get('/images/:id', (req, res) => {
-  res.sendFile(sendImage(req.params.id), { root: '.' })
-})
+  res.sendFile(sendImage(req.params.id), { root: '.' });
+});
 
 // Sends an icon for an NFT.
 const sendIcon = function (id) {
   if (nftMinted(id)) {
-    return 'data/icons/' + id + '.png'
+    return 'data/icons/' + id + '.png';
   } else {
-    return 'images/unknown-icon.webp'
+    return 'images/unknown-icon.webp';
   }
-}
+};
 
 // NFT icon route.
 app.get('/icons/:id', (req, res) => {
-  res.sendFile(sendIcon(req.params.id), { root: '.' })
-})
+  res.sendFile(sendIcon(req.params.id), { root: '.' });
+});
 
 // Filters BCMR down to a single NFT record.
 const bcmrNFT = function (id, bcmr) {
   if (bcmr.identities[tokenId]) {
     if (nftMinted(id)) {
-      let vmNum = bigIntToVmNumber(BigInt(id - 1))
-      let indexHex = binToHex(vmNum)
+      let vmNum = bigIntToVmNumber(BigInt(id - 1));
+      let indexHex = binToHex(vmNum);
 
       if (hideIPFSImages) {
         if (bcmr.identities[tokenId][mintDate]["token"]["nfts"]["parse"]["types"][indexHex]) {
-          bcmr.identities[tokenId][mintDate]["token"]["nfts"]["parse"]["types"][indexHex]["uris"]["image"] = apiDomain + "images/" + (id)
+          bcmr.identities[tokenId][mintDate]["token"]["nfts"]["parse"]["types"][indexHex]["uris"]["image"] = apiDomain + "images/" + (id);
         }
       }
       if (hideIPFSIcons) {
         if (bcmr.identities[tokenId][mintDate]["token"]["nfts"]["parse"]["types"][indexHex]) {
-          bcmr.identities[tokenId][mintDate]["token"]["nfts"]["parse"]["types"][indexHex]["uris"]["icon"] = apiDomain + "icons/" + (id)
+          bcmr.identities[tokenId][mintDate]["token"]["nfts"]["parse"]["types"][indexHex]["uris"]["icon"] = apiDomain + "icons/" + (id);
         }
       }
 
-      return bcmr.identities[tokenId][mintDate]["token"]["nfts"]["parse"]["types"][indexHex]
+      return bcmr.identities[tokenId][mintDate]["token"]["nfts"]["parse"]["types"][indexHex];
     }
   }
 
   return {};
-}
+};
 
 // NFT metadata route.
 app.get('/nfts/:id', (req, res) => {
   res.json(bcmrNFT(req.params.id, bcmrJSON));
-})
+});
 
 // Start server.
 app.listen(port, () => {
